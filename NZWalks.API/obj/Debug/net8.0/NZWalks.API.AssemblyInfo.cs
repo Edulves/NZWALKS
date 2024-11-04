d5aad28ed76fecc1133528ff953c9e0d7c4955aa
Eduardo <eduardo.lves@outlook.com>
2024-11-03 23:19:10 -0300

    Finished

--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c9d7a255056a08f85b0eaf1c765bc06b3aa78ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e86d0ea418b7a089c3e616851767cee0bcee72b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
